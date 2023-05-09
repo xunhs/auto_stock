8fc8606e9c21294fd7eb2d625dd7ec30626ab732
Ethan <31309468+xunhs@users.noreply.github.com>
2023-05-09 11:20:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -302,9 +302,9 @@ index_html_str = '''
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://ghproxy.net/https://raw.githubusercontent.com/xunhs/auto_stock/main/bootstrap.min.css" rel="stylesheet">
     <title>Auto Stock</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs/dist/css/autoc.min.css">
+    <link href="https://ghproxy.net/https://raw.githubusercontent.com/xunhs/auto_stock/main/autoc.min.css" rel="stylesheet" >
 </head>
 <body>
     <div class="container">
@@ -488,7 +488,8 @@ html_end_str = '''
     </div>
 
 
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yaohaixiao/autocjs/dist/autoc.min.js"></script>
+    <script type="text/javascript" src="https://ghproxy.net/https://raw.githubusercontent.com/xunhs/auto_stock/main/autoc.min.js"></script>
+    <script src="https://ghproxy.net/https://raw.githubusercontent.com/xunhs/auto_stock/main/bootstrap.min.js"></script>
     <script type="text/javascript">
          new AutocJs({
             article: '#wrap',
@@ -496,7 +497,6 @@ html_end_str = '''
             position: 'outside',
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
 
